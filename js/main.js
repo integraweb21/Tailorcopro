2e80f0f60121e461131a86c24a3306302a171002
integra-web <contact@integra-web.net>
2022-05-24 11:08:52 +0200

    Testimonials

--- js/main.js
+++ js/main.js
@@ -2,7 +2,24 @@ $(document).ready(function(){
 
     // Owl Carousel
 
-    $('.owl-carousel').owlCarousel({
+    $('.testimonials').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+
+    $('.partners').owlCarousel({
         loop:true,
         margin:10,
         nav:true,
